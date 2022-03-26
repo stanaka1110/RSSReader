52ca5e9b5ca9479663663fa59026640144499588
stanaka1110 <tnkshn1110@gmail.com>
2022-03-26 21:08:47 +0000

    add detail view

--- config/rss/views.py
+++ config/rss/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View 
-from .models import Link
-from django.http import HttpResponse
+from .models import Link, Article
+from .rss_parser import parse
 # Create your views here.
 class IndexView(View):
 
@@ -14,5 +14,12 @@ class IndexView(View):
 index = IndexView.as_view()
 
 def detail(request, pk):
-    context = {test:"test"}
-    return render(request, "rss/index.html", context)
+    site = get_object_or_404(Link, pk=pk)
+    article_list = parse(site.url)
+    for a in article_list:
+        article = Article.objects.create(title=a['title'], url=a['url'])
+    
+    articles = Article.objects.all()
+    context = {"articles":articles}
+    return render(request, "rss/detail.html", context)
+
